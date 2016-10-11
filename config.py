606d44da13de7af8e613f8da58874789ce3305cd
nguyen.h.nam.vn@gmail.com <nguyen.h.nam.vn@gmail.com>
2016-10-11 14:50:49 +0700

    Change config directory of MOC

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os 
 SAVE_LOCATION = '/tmp'
 
-MOC_DIR = '/home/music_aekt/.moc'
-MOC_PLAYLIST = '/home/music_aekt/.moc/playlist.m3u'
+HOME_DIRECTORY = os.environ['HOME']
+MOC_DIRECTORY = HOME_DIRECTORY + '/.moc'
+MOC_PLAYLIST = MOC_DIRECTORY + '/playlist.m3u'
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
