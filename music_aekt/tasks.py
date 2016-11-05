857fe04641427ca1a9db6d763e2b1fb774ca57c1
Nguyen Hoang Nam <nguyen.h.nam.vn@gmail.com>
2016-11-05 09:17:38 +0700

    Change pattern to get mp3 url

--- music_aekt/tasks.py
+++ music_aekt/tasks.py
@@ -11,8 +11,10 @@ SAVE_LOCATION = '/tmp'
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
 
+PATTERN = 'var xml_link = "(.*?)";'
+
 @app.task
 def download(url):
-    z = ZingDownloader(url=url, path=SAVE_LOCATION, headers=HEADERS) 
+    z = ZingDownloader(url=url, path=SAVE_LOCATION, headers=HEADERS, pattern = PATTERN)
     f = z.download_mp3_file()
     moc.append(f)
