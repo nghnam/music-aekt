8540ae9299bec4041ac54f2078099f9fe76c11ea
Nguyen Hoang Nam <nguyen.h.nam.vn@gmail.com>
2016-11-10 10:40:42 +0700

    Add NCT downloader for celery tasks

--- music_aekt/tasks.py
+++ music_aekt/tasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
 from music_aekt.downloaders.zing import ZingDownloader
+from music_aekt.downloaders.nhaccuatui import NCTDownloader
 from music_aekt.player import moc
 
 app = Celery('tasks',
@@ -11,10 +12,18 @@ SAVE_LOCATION = '/tmp'
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
 
-PATTERN = 'var xml_link = "(.*?)";'
+ZING_PATTERN = 'var xml_link = "(.*?)";'
 
 @app.task
 def download(url):
-    z = ZingDownloader(url=url, path=SAVE_LOCATION, headers=HEADERS, pattern = PATTERN)
-    f = z.download_mp3_file()
+    if "mp3.zing.vn" in url:
+        d = ZingDownloader(url=url,
+                           path=SAVE_LOCATION,
+                           headers=HEADERS,
+                           pattern = ZING_PATTERN)
+    elif "nhaccuatui.com" in url:
+        d = NCTDownloader(url=url,
+                          path=SAVE_LOCATION,
+                          headers=HEADERS)
+    f = d.download_mp3_file()
     moc.append(f)
