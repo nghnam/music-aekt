e2eb0a7c48ecbde63dcf84f348049f3802de3aac
Nguyen Hoang Nam <nguyen.h.nam.vn@gmail.com>
2016-11-19 10:35:50 +0700

    Add configs for http auth

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ MOC_DIRECTORY = HOME_DIRECTORY + '/.moc'
 MOC_PLAYLIST = MOC_DIRECTORY + '/playlist.m3u'
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
+
+USERS = {'username here': 'password here'}
+
+SECRET_KEY = 'secret key here'
