8dcbe59648e5bfcc06e1b3740243eec1506635ef
Nguyen Hoang Nam <nguyen.h.nam.vn@gmail.com>
2018-06-11 17:43:08 +0700

    Add test multiply

--- test/test_unittest.py
+++ test/test_unittest.py
@@ -8,6 +8,9 @@ class TestUnitTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(5+7, 12)
  
+    def test_multiply(self):
+        self.assertEqual(2*2, 4)
+
 if __name__ == '__main__':
     unittest.main()
 
