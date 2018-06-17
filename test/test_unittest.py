8271364b747ee937160bba287f03cd836773676c
Nguyen Hoang Nam <nguyen.h.nam.vn@gmail.com>
2018-06-17 09:21:35 +0700

    Add test sub

--- test/test_unittest.py
+++ test/test_unittest.py
@@ -7,6 +7,9 @@ class TestUnitTest(unittest.TestCase):
  
     def test_add(self):
         self.assertEqual(5+7, 12)
+
+    def test_sub(self):
+        self.assertEqual(1-1, 0)
  
 if __name__ == '__main__':
     unittest.main()
